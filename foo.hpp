ce4ed2ab0f0a7adc47bfa1acc5cf5a9e1804b68e
xSzumielx <szumielbartek@onet.pl>
2025-11-27 11:27:12 +0100

    zrobiona zejściówka

--- foo.hpp
+++ foo.hpp
@@ -5,12 +5,24 @@
 #include <algorithm>
 #include <list>
 #include <vector>
+#include <iostream>
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    std::vector< char > retval(people.size());
+    std::vector< char > retval;
+    retval.reserve(people.size());
 
-    // Twoja implementacja tutaj
+    auto                f = [&](Human& person) {
+        person.birthday();
+        if (person.isMonster() == true) {
+            retval.push_back('n');
+        }
+        else {
+            retval.push_back('y');
+        }
+    };
+    std::for_each(people.begin(), people.end(), f);
+    std::reverse(retval.begin(), retval.end());
 
     return retval;
 }
